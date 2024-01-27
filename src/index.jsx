7f5776e5e6c311e98b884a03cd4d04c861331b91
nugrohoodik90 <nugrohoodik90@gmail.com>
2024-01-27 07:45:54 +0700

    update template project

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+import Admin from "./layouts/Admin"
+import Landing from "./views/index"
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./assets/styles/tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,8 +15,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        "test"
-      </BrowserRouter>,
+        <Routes>
+          <Route path="/admin/dasboard" element={<Admin />}/>
+          <Route path="/" element={<Landing/>}/>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -1,35 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
 
-// components
-
-import AdminNavbar from "components/Navbars/AdminNavbar.js";
-import Sidebar from "components/Sidebar/Sidebar.js";
-import Header from "components/Headers/Header.js";
-//import FooterAdmin from "components/Footers/FooterAdmin.js";
-
-// views
-
-// import Dashboard from "views/admin/Dashboard.js";
-// import Maps from "views/admin/Maps.js";
-// import Settings from "views/admin/Settings.js";
-// import Tables from "views/admin/Tables.js";
+import Sidebar from "../components/Sidebar";
 
 export default function Admin() {
   return (
     <>
       <Sidebar />
       <div className="relative md:ml-64 bg-blueGray-100">
-        <AdminNavbar />
-        {/* Header */}
-        <Header />
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
-          "Hello"
-            {/*   <Redirect from="/admin" to="/admin/dashboard" />
-          </Switch>
-          {/* <FooterAdmin /> */}
-        </div>
+       </div>
       </div>
     </>
   );
