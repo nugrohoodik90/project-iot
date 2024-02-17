c91afb0e7c18e247a50347c995db7cbec7d3545f
nugrohoodik90 <nugrohoodik90@gmail.com>
2024-02-17 19:51:29 +0700

    update setup for production

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -6,7 +6,8 @@ const store = createStore(
     reducers,
     compose(
       applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      // redux toolkit just for dev, if not not include
+      // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
   );
 export default store
