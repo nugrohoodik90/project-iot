4acf36d6fff3ad0658b2df5739d11b4306b57927
o-dik90 <odikyudi69@gtmail.com>
2024-04-14 14:49:22 +0700

    upfstr feature

--- src/redux/actions/relayActions/index.jsx
+++ src/redux/actions/relayActions/index.jsx
@@ -1,25 +1,33 @@
 import axios from "axios"
 
-export function relaysActions() {
-    return dispatch => {
-        axios.get("https://cyclic-rest-iot.onrender.com/rel-get/").then(res => {
+export const relaysActions = () => {
+    return async (dispatch) => {
+        const res = await axios.get("https://cyclic-rest-iot.onrender.com/rel-get/6603410169bdae33c35a3247")
+
+        if (res.status === 200) {
             dispatch({
-                type: "RELAYS",
+                type : "RELAYS",
                 payload: res.data
-            });
-        }).catch(err => {
-            console.log("error", err);
-        })
+            })
+        }
     }
 }
 
-export const updateRelaysActions = async ({data}) => {
-    await axios.put("https://cyclic-rest-iot.onrender.com/", { 
+export const updateRelaysActions = (data) => {
+    console.log(data)
+    axios.put("https://cyclic-rest-iot.onrender.com/rel-update/6603410169bdae33c35a3247", { 
         description: data.description,
-        value: Boolean(data.value) ,
-        duration : data.duration
+        status: Boolean(data.status) ,
+        duration : data.duration,
+        load_1: data.load_1,
+        load_2: data.load_2,
+        load_3: data.load_3,
+        time_1: data.time_1,
+        time_2: data.time_3,
+        time_3: data.time_3
     }).then(res => {
         console.log(`${res.status}-success`);
+        console.log(res.data);
     }).catch(err => {
         console.log("error", err);
     })
