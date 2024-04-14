e3100077db868cfa3c254d7784a767a803a55ce8
o-dik90 <odikyudi69@gtmail.com>
2024-04-14 17:14:40 +0700

    update

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -7,6 +7,7 @@ const store = createStore(
     compose(
       applyMiddleware(thunk),
       // redux toolkit just for dev, if not not include
+      //update
       //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
   );
