ea99901b8cb9b7f41791921953c6a33b3ceed31f
o-dik90 <odikyudi69@gtmail.com>
2024-04-14 17:20:17 +0700

    update

--- src/components/graphic/CardLineChart.jsx
+++ src/components/graphic/CardLineChart.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Chart from "chart.js";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import termoActions from "../../redux/actions/termoActions";
 
 export default function CardLineChart() {
